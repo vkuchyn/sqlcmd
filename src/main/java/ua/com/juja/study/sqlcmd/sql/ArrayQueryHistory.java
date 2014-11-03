17d93e88eb2c5a4679b84accf166e7454bc3f55e
Bagzoid <a.kudlaenko@gmail.com>
2014-11-03 21:17:15 +0200

    Исправления по замечанию: переход к использованию массива вместо коллекции

--- src/main/java/ua/com/juja/study/sqlcmd/sql/ArrayQueryHistory.java
+++ src/main/java/ua/com/juja/study/sqlcmd/sql/ArrayQueryHistory.java
@@ -1,65 +1,45 @@
 package ua.com.juja.study.sqlcmd.sql;
 
-import java.util.ArrayList;
-
 /**
  * Created by Artem on 26.10.2014.
  */
 public class ArrayQueryHistory implements QueryHistory {
 
-    private ArrayList<String> queryList = new ArrayList<String>(QUERY_BUFFER_COUNT);
-    public static final int QUERY_BUFFER_COUNT = 20;
+    int QUERY_BUFFER_COUNT = 3;
+    private String[] queryList = new String[QUERY_BUFFER_COUNT];
     private int currentQueryIndex = -1;
+    private int queryCount = 0;
 
     @Override
     public String getNextQuery() {
-        if (!queryList.isEmpty() && queryList.size() - 1 > currentQueryIndex) {
-            return queryList.get(++currentQueryIndex);
+        if (queryCount > 0 && queryCount - 1 > currentQueryIndex) {
+            return queryList[++currentQueryIndex];
         }
         return null;
     }
 
     @Override
     public String getPreviousQuery() {
-        if (!queryList.isEmpty() && currentQueryIndex > 0) {
-            return queryList.get(--currentQueryIndex);
+        if (queryCount > 0 && currentQueryIndex > 0) {
+            return queryList[--currentQueryIndex];
         }
         return null;
     }
 
     @Override
     public void addQueryToTheHead(String query) {
-        // If query list is full remove last query
-        if (queryList.size() + 1 >= QUERY_BUFFER_COUNT) {
-            queryList.remove(QUERY_BUFFER_COUNT - 1);
+        addQueryToTheHead(queryList, query);
+        if (queryCount < QUERY_BUFFER_COUNT) {
+            queryCount++;
         }
-        // Add query to the head
-        // TODO: ArrayList add to the head is low performance operation. Replace with Queue.
-        queryList.add(0, query);
-
         // Reset query index
         currentQueryIndex = -1;
     }
 
-    public static void main(String[] args) {
-        ArrayQueryHistory queryhistory = new ArrayQueryHistory();
-        queryhistory.addQueryToTheHead("Query 1");
-        queryhistory.addQueryToTheHead("Query 2");
-        queryhistory.addQueryToTheHead("Query 3");
-        queryhistory.addQueryToTheHead("Query 4");
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        queryhistory.addQueryToTheHead("Query 5");
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getNextQuery());
-        System.out.println(queryhistory.getPreviousQuery());
+    private void addQueryToTheHead(String[] queryList, String query) {
+        for (int i = queryList.length - 2; i >= 0; i--) {
+            queryList[i + 1] = queryList[i];
+        }
+        queryList[0] = query;
     }
 }
