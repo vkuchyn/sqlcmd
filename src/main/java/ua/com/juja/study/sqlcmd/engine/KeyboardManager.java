ee6855329c526fa6d8e1aa08ac3bcef2c74386ea
i-kostenko <flyleaf248@gmail.com>
2014-11-04 21:10:19 +0200

    Some changes

--- src/main/java/ua/com/juja/study/sqlcmd/engine/KeyboardManager.java
+++ src/main/java/ua/com/juja/study/sqlcmd/engine/KeyboardManager.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class KeyboardManager {
+public class KeyboardManager{
     private QueryHistory history;
     private DatabaseExecutor databaseExecutor;
 
