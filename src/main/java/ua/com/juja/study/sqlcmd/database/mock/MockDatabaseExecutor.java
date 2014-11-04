e35e8af972918645498c1fc17990b53ecedca173
i-kostenko <flyleaf248@gmail.com>
2014-11-04 17:29:00 +0200

    Some changes

--- src/main/java/ua/com/juja/study/sqlcmd/database/mock/MockDatabaseExecutor.java
+++ src/main/java/ua/com/juja/study/sqlcmd/database/mock/MockDatabaseExecutor.java
@@ -2,14 +2,17 @@ package ua.com.juja.study.sqlcmd.database.mock;
 
 import ua.com.juja.study.sqlcmd.config.SqlCmdConfig;
 import ua.com.juja.study.sqlcmd.database.DatabaseExecutor;
+import ua.com.juja.study.sqlcmd.database.Row;
+import ua.com.juja.study.sqlcmd.util.Utils;
 
-/**
- * Created with IntelliJ IDEA.
- * User: viktor
- * Date: 10/15/14
- * Time: 11:42 PM
- */
 public class MockDatabaseExecutor implements DatabaseExecutor {
+
+    private static final String [] MOCK_DATABASES = {"juja", "java", "database1"};
+    private int currentDatabaseIndex = 0;
+
+    public String getCurrentDatabase(){
+        return  MOCK_DATABASES[currentDatabaseIndex];
+    }
 
     @Override
     public boolean connectToDb(SqlCmdConfig config) {
@@ -17,4 +20,43 @@ public class MockDatabaseExecutor implements DatabaseExecutor {
         return true;
     }
 
+    @Override
+    public Row[] executeSqlScript(String sqlScript) {
+        System.out.println("Executed script " + sqlScript);
+        return new Row[0];
+    }
+
+    @Override
+    public String[] getDatabaseList() {
+        return MOCK_DATABASES;
+    }
+
+    @Override
+    public void changeDatabase(String databaseName) {
+        boolean databaseWasFound = false;
+        for (int i = 0; i < MOCK_DATABASES.length; i++){
+            if(MOCK_DATABASES[i].equals(databaseName)){
+                currentDatabaseIndex = i;
+                databaseWasFound = true;
+                break;
+            }
+        }
+            if (!databaseWasFound){
+                System.out.println("Wrong database name " + databaseName);
+            }
+
+    }
+
+    public static void main(String[] args) {
+        MockDatabaseExecutor executor = new MockDatabaseExecutor();
+        String databases = Utils.arrayToString(executor.getDatabaseList());
+        System.out.println("Databases are " + databases);
+        System.out.println("Current database is " + executor.getCurrentDatabase());
+        executor.changeDatabase("java");
+        System.out.println("Current database is " + executor.getCurrentDatabase());
+        executor.changeDatabase("database1");
+        System.out.println("Current database is " + executor.getCurrentDatabase());
+        executor.changeDatabase("database2");
+        System.out.println("Current database is " + executor.getCurrentDatabase());
+    }
 }
--- src/main/java/ua/com/juja/study/sqlcmd/engine/KeyboardManager.java
+++ src/main/java/ua/com/juja/study/sqlcmd/engine/KeyboardManager.java
@@ -4,18 +4,34 @@ import ua.com.juja.study.sqlcmd.database.DatabaseExecutor;
 import ua.com.juja.study.sqlcmd.database.Row;
 import ua.com.juja.study.sqlcmd.sql.QueryHistory;
 
-/**
- * Created with IntelliJ IDEA.
- * User: viktor
- * Date: 10/22/14
- * Time: 11:28 PM
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class KeyboardManager {
     private QueryHistory history;
     private DatabaseExecutor databaseExecutor;
 
+    public KeyboardManager(QueryHistory history, DatabaseExecutor databaseExecutor) {
+        this.history = history;
+        this.databaseExecutor = databaseExecutor;
+    }
+
     public Row[] executeQuery(String query) {
         history.addQueryToTheHead(query);
         return databaseExecutor.executeSqlScript(query);
+    }
+
+    public void startListenUserKeyboard() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean quit = false;
+        while (!quit){
+            String query = reader.readLine();
+            if ("\\q".equals((query))){
+                quit = true;
+            } else {
+                executeQuery(query);
+            }
+        }
     }
 }
