d9ba81edc05ffd82564efaceb6198bc8d2347460
Bagzoid <a.kudlaenko@gmail.com>
2014-11-06 21:08:47 +0200

    Рефакторинг кода

--- src/main/java/ua/com/juja/study/sqlcmd/engine/KeyboardManager.java
+++ src/main/java/ua/com/juja/study/sqlcmd/engine/KeyboardManager.java
@@ -7,6 +7,7 @@ import ua.com.juja.study.sqlcmd.sql.QueryHistory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,9 +16,48 @@ import java.io.InputStreamReader;
  * Time: 11:28 PM
  */
 public class KeyboardManager {
-    private static final String QUERY_NEWLINE_SEPARATOR = " ";
     private QueryHistory history;
     private DatabaseExecutor databaseExecutor;
+
+    private class QueryReader {
+        private static final String QUERY_NEWLINE_SEPARATOR = " ";
+        private final BufferedReader reader;
+
+        public QueryReader(Reader in) {
+            reader = new BufferedReader(in);
+        }
+
+        public String read() throws IOException {
+            boolean exit = false;
+            StringBuilder query = new StringBuilder();
+            while (!exit) {
+                String line = reader.readLine();
+                if ("\\q".equals(line)) {
+                    query = new StringBuilder();
+                    exit = true;
+                } else if (line != null && line.length() > 0) {
+                    if (query.length() > 0) {
+                        query.append(QUERY_NEWLINE_SEPARATOR);
+                    }
+                    query.append(line);
+                    if (isQueryFinalized(query.toString())) {
+                        exit = true;
+                    }
+                }
+            }
+            return query.toString();
+        }
+
+        private boolean isQueryFinalized(String query) {
+            boolean result = false;
+            if (null != query
+                    && query.length() > 0
+                    && query.charAt(query.length() - 1) == ';') {
+                result = true;
+            }
+            return result;
+        }
+    }
 
     public KeyboardManager(QueryHistory queryHistory, DatabaseExecutor databaseExecutor) {
         this.history = queryHistory;
@@ -25,28 +65,16 @@ public class KeyboardManager {
     }
 
     public void startListenUserKeyboard() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        boolean exit = false;
-        StringBuilder query = new StringBuilder();
-        while (!exit) {
-            String line = reader.readLine();
-            if ("\\q".equals(line)) {
-                exit = true;
-            } else if (line != null && line.length() > 0){
-                query.append(line);
-                char lastChar = line.charAt(line.length() - 1);
-                if (lastChar == ';') {
-                    executeQuery(query.toString());
-                    query = new StringBuilder();
-                } else {
-                    query.append(QUERY_NEWLINE_SEPARATOR);
-                }
-            }
+        QueryReader queryReader = new QueryReader(new InputStreamReader(System.in));
+        String query = queryReader.read();
+        while (query.length() > 0) {
+            executeQuery(query);
+            query = queryReader.read();
         }
     }
 
-
     public Row[] executeQuery(String query) {
+        System.out.println("Executing: " + query);
         history.addQueryToTheHead(query);
         return databaseExecutor.executeSqlScript(query);
     }
