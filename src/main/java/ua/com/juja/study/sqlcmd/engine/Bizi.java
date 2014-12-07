4675cb2482d4d3a1e5967d9143a316ce7bbaf11e
bizon4ik <Gjljkzrj85>
2014-12-07 17:38:53 +0200

    Linux test

--- src/main/java/ua/com/juja/study/sqlcmd/engine/Bizi.java
+++ src/main/java/ua/com/juja/study/sqlcmd/engine/Bizi.java
@@ -4,13 +4,15 @@ import jline.ConsoleReader;
 
 import java.io.IOException;
 
-public class Biz {
+// It's a mirracl
+
+public class Bizi {
     public static void main(String[] args) throws IOException {
         ConsoleReader consoleReader = new ConsoleReader();
         String line;
         StringBuilder queryBuilder = new StringBuilder();
         while (!(line = consoleReader.readLine()).equals("\\q")) {
-            queryBuilder.append(line).append(" ");
+
 
         }
         System.out.println("Query = " + queryBuilder.toString());
---
+++ src/main/java/ua/com/juja/study/sqlcmd/engine/test.java
@@ -0,0 +1,7 @@
+package ua.com.juja.study.sqlcmd.engine;
+
+/**
+ * Created by bizon4ik on 07.12.14.
+ */
+public class test {
+}
